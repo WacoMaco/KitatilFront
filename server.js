cd2b392a2f006bc8f1f44db4fc00ce873bd9516c
Waco <wacomacaco@gmail.com>
2019-11-11 20:45:09 +0100

    Build

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ let path = require('path');
 let serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-app.use(history({
-    disableDotRule: true,
-    verbose: true
-  }));
+app.use(history());
 let port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
