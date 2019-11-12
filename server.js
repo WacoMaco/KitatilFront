d8aba4362312cae9ef10f0928451353b0c9b4bab
Waco <wacomacaco@gmail.com>
2019-11-12 01:03:54 +0100

    Configuracion de serve

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ let express = require('express');
 let path = require('path');
 let serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+
+app.use(express.static(__dirname + "/dist"));
 app.use(history());
 let port = process.env.PORT || 5000;
 app.listen(port);
