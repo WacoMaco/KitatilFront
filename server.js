7e905d3f9f6aa031081cebc0bd54bf6108db3c96
Waco <wacomacaco@gmail.com>
2019-11-12 01:11:48 +0100

    Configuracion de serve

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var history = require('connect-history-api-fallback');
 let express = require('express');
 let path = require('path');
 let serveStatic = require('serve-static');
-app = express();
-
-app.use(express.static(__dirname + "/dist"));
-app.use(history());
+const app = express();
+app.use(history({
+    verbose: true
+}));
+app.use('/',express.static(__dirname + "/dist"));
 let port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
